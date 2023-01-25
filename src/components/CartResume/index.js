ecd948e9a89e67c9da637bf0bd81c3856a591cdb
Guilherme Raposo <gui.raposo36@gmail.com>
2023-01-25 00:10:13 -0300

    feat: implements requisition from order

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 import { useCart } from '../../hooks/CardContext'
+import api from '../../services/api'
 import formatCurrency from '../../utils/formatCurrency'
 import { Button } from '../Button'
 import { Container, ContainerItems, ContainerPrice } from './styles'
@@ -17,6 +19,19 @@ export function CartResume() {
 
     setFinalPrice(sumAllItems)
   }, [cartProducts])
+
+  const submitOrder = async () => {
+    if (cartProducts.length === 0) return
+    const order = cartProducts.map(product => {
+      return { id: product.id, quantity: product.quantity }
+    })
+
+    await toast.promise(api.post('orders', { products: order }), {
+      pending: 'Realizando o seu pedido...',
+      success: 'Pedido realizado com sucesso',
+      error: 'Falha ao tentar realizar o seu pedido, tente novamente'
+    })
+  }
 
   return (
     <div>
@@ -35,7 +50,11 @@ export function CartResume() {
         </ContainerPrice>
       </Container>
 
-      <Button style={{ width: '100%', marginTop: '30px' }}>
+      <Button
+        style={{ width: '100%', marginTop: '30px' }}
+        disable={cartProducts.length === 0}
+        onClick={submitOrder}
+      >
         Finalizar pedido
       </Button>
     </div>
