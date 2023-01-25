8442b7a20440e6685fd102e096543bf04503556a
Guilherme Raposo <gui.raposo36@gmail.com>
2023-01-24 22:22:01 -0300

    feat: implements cart style

--- src/components/CartItems/index.js
+++ src/components/CartItems/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useCart } from '../../hooks/CardContext'
 import formatCurrency from '../../utils/formatCurrency'
-import { Body, Container, Header } from './styles'
+import { Body, Container, EmptyCart, Header } from './styles'
 
 export function CartItems() {
   const { cartProducts } = useCart()
@@ -13,11 +13,11 @@ export function CartItems() {
         <p></p>
         <p>Itens</p>
         <p>Preço</p>
-        <p>Quantidade</p>
+        <p style={{ paddingRight: 30 }}>Quantidade</p>
         <p>Total</p>
       </Header>
 
-      {cartProducts &&
+      {cartProducts && cartProducts.length > 0 ? (
         cartProducts.map(product => (
           <Body key={product.id}>
             <img src={product.url} />
@@ -26,7 +26,10 @@ export function CartItems() {
             <p>{product.quantity}</p>
             <p>{formatCurrency(product.quantity * product.price)}</p>
           </Body>
-        ))}
+        ))
+      ) : (
+        <EmptyCart>Carrinho vazio</EmptyCart>
+      )}
     </Container>
   )
 }
