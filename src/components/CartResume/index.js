772a749e9843be7e7284d252eefb4d0f5a6ea3c4
Guilherme Raposo <gui.raposo36@gmail.com>
2023-01-24 23:52:56 -0300

    feat: implements sum prices from cart resume

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import { useCart } from '../../hooks/CardContext'
 import formatCurrency from '../../utils/formatCurrency'
 import { Button } from '../Button'
 import { Container, ContainerItems, ContainerPrice } from './styles'
 
 export function CartResume() {
+  const [finalPrice, setFinalPrice] = useState(0)
+  const [deliveryTax] = useState(10)
+  const { cartProducts } = useCart()
+
+  useEffect(() => {
+    const sumAllItems = cartProducts.reduce((acc, current) => {
+      return current.price * current.quantity + acc
+    }, 0)
+
+    setFinalPrice(sumAllItems)
+  }, [cartProducts])
+
   return (
     <div>
       <Container>
         <ContainerItems>
           <h2 className="title">Resumo do pedido</h2>
           <p className="items">Itens</p>
-          <p className="items-price">{formatCurrency(10)}</p>
+          <p className="items-price">{formatCurrency(finalPrice)}</p>
           <p className="delivery-tax">Taxa de entrega</p>
-          <p className="delivery-tax-price">{formatCurrency(10)}</p>
+          <p className="delivery-tax-price">{formatCurrency(deliveryTax)}</p>
         </ContainerItems>
 
         <ContainerPrice>
           <p>Total</p>
-          <p>{formatCurrency(100)}</p>
+          <p>{formatCurrency(finalPrice + deliveryTax)}</p>
         </ContainerPrice>
       </Container>
 
