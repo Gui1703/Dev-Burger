ec63a12706fea0e022db061b4b54278b413778be
Guilherme Raposo <gui.raposo36@gmail.com>
2023-01-30 23:38:12 -0300

    feat: implements form in new product

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import ReactSelect from 'react-select'
 
+import { Button } from '../../../components'
 import api from '../../../services/api'
-import { Container } from './styles'
+import { Container, Input, Label } from './styles'
 
 export default function NewProduct() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
+
   useEffect(() => {
     async function loadOrders() {
       await api.post('products')
@@ -12,7 +18,20 @@ export default function NewProduct() {
 
   return (
     <Container>
-      <h1>Hello World</h1>
+      <form noValidate onSubmit={onSubmit}>
+        <Label>Produto</Label>
+        <Input type="text" {...register('productName')} />
+
+        <Label>Preço</Label>
+        <Input type="number" {...register('price')} />
+
+        <Label>Upload</Label>
+        <Input type="file" accept="image/png, image/jpeg" />
+
+        <ReactSelect />
+
+        <Button type="submit">Adicionar Produto</Button>
+      </form>
     </Container>
   )
 }
